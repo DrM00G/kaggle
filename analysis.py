5f2a29ebcb484e4a7193da84f3794f174eface2d
Dr. Moo <>
2021-04-16 11:03:00 +0000

    Got impatient with following the instructions for 108, Ill finish next time, although not quite sure whats wrong.

--- analysis.py
+++ analysis.py
@@ -1,7 +1,6 @@
-import pandas 
+import pandas as pd 
 import numpy as np
 from sklearn.linear_model import LinearRegression, LogisticRegression
-import sys
 
 df = pd.read_csv('/home/runner/kaggle/dataset.csv')
 
@@ -18,6 +17,8 @@ def convert_sex_to_int(sex):
         return 1
 
 df['Sex'] = df['Sex'].apply(convert_sex_to_int)
+
+# print(df)
 
 #age
 age_nan = df['Age'].apply(lambda entry: np.isnan(entry))
@@ -39,76 +40,76 @@ df['SibSp>0'] = df['SibSp'].apply(indicator_greater_than_zero)
 # parch
 df['Parch>0'] = df['Parch'].apply(indicator_greater_than_zero)
 
-print(df)
+# print(df)
 # CabinType
 
-# df['Cabin']= df['Cabin'].fillna('None')
+df['Cabin']= df['Cabin'].fillna('None')
 
-# def get_cabin_type(cabin):
-#     if cabin != 'None':
-#         return cabin[0]
-#     else:
-#         return cabin
+def get_cabin_type(cabin):
+    if cabin != 'None':
+        return cabin[0]
+    else:
+        return cabin
 
-# df['CabinType'] = df['Cabin'].apply(get_cabin_type)
+df['CabinType'] = df['Cabin'].apply(get_cabin_type)
 
-# for cabin_type in df['CabinType'].unique():
-#     dummy_variable_name = 'CabinType={}'.format(cabin_type)
-#     dummy_variable_values = df['CabinType'].apply(lambda entry: int(entry==cabin_type))
-#     df[dummy_variable_name] = dummy_variable_values
+for cabin_type in df['CabinType'].unique():
+    dummy_variable_name = 'CabinType={}'.format(cabin_type)
+    dummy_variable_values = df['CabinType'].apply(lambda entry: int(entry==cabin_type))
+    df[dummy_variable_name] = dummy_variable_values
 
-# del df['CabinType']
+del df['CabinType']
 
-# # Embarked
+# Embarked
 
-# df['Embarked'] = df['Embarked'].fillna('None')
+df['Embarked'] = df['Embarked'].fillna('None')
 
-# for cabin_type in df['Embarked'].unique():
-#     dummy_variable_name = 'Embarked={}'.format(cabin_type)
-#     dummy_variable_values = df['Embarked'].apply(lambda entry: int(entry==cabin_type))
-#     df[dummy_variable_name] = dummy_variable_values
+for cabin_type in df['Embarked'].unique():
+    dummy_variable_name = 'Embarked={}'.format(cabin_type)
+    dummy_variable_values = df['Embarked'].apply(lambda entry: int(entry==cabin_type))
+    df[dummy_variable_name] = dummy_variable_values
 
-# del df['Embarked']
+del df['Embarked']
 
-# #print(df)
-
-
-# #print(df['CabinType'])
+# print(df)
 
 
-# features_to_use = ['Sex', 'Pclass', 'Fare', 'Age', 'SibSp', 'SibSp>0', 'Parch>0', 'Embarked=C', 'Embarked=None', 'Embarked=Q', 'Embarked=S', 'CabinType=A', 'CabinType=B', 'CabinType=C', 'CabinType=D', 'CabinType=E', 'CabinType=F', 'CabinType=G', 'CabinType=None', 'CabinType=T']
+#print(df['CabinType'])
 
-# #print(df.columns)
 
-# # split into training/testing dataframes
-# num_train = 500
-# df_train = df[:num_train]
-# df_test = df[num_train:]
+features_to_use = ['Sex', 'Pclass', 'Fare', 'Age', 'SibSp', 'SibSp>0', 'Parch>0', 'Embarked=C', 'Embarked=None', 'Embarked=Q', 'Embarked=S', 'CabinType=A', 'CabinType=B', 'CabinType=C', 'CabinType=D', 'CabinType=E', 'CabinType=F', 'CabinType=G', 'CabinType=None', 'CabinType=T']
 
-# arr_train = np.array(df_train[df_train])
-# arr_test = np.array(df_test[df_test])
+#print(df.columns)
 
-# y_train = arr_train[:,0]
-# y_test = arr_test[:,0]
+# split into training/testing dataframes
+num_train = 500
+df_train = df[:num_train]
+df_test = df[num_train:]
 
-# X_train = arr_train[:,1:]
-# X_test = arr_test[:,1:]
+arr_train = np.array(df_train[df_train])
+arr_test = np.array(df_test[df_test])
 
-# regressor = LinearRegression()
-# regressor.fit(X_train, y_train)
+y_train = arr_train[:,0]
+y_test = arr_test[:,0]
 
-# coef_dict = {}
-# feature_columns = df_train.columns[1:]
-# feature_coefficients = regressor.coef_[0]
-# #print('feature_coefficients', feature_coefficients)
-# #print(feature_coefficients)
-# for i in range(len(feature_columns)):
-#         column = feature_columns[i]
-#         coefficient = feature_coefficients[i]
-#         coef_dict[column] = coefficient
+X_train = arr_train[:,1:]
+X_test = arr_test[:,1:]
 
-# y_test_predictions = regressor.predict(X_test)
-# print(y_test_predictions)
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+coef_dict = {}
+feature_columns = df_train.columns[1:]
+feature_coefficients = regressor.coef_[0]
+#print('feature_coefficients', feature_coefficients)
+#print(feature_coefficients)
+for i in range(len(feature_columns)):
+        column = feature_columns[i]
+        coefficient = feature_coefficients[i]
+        coef_dict[column] = coefficient
+
+y_test_predictions = regressor.predict(X_test)
+print(y_test_predictions)
 # y_train_predictions = regressor.predict(X_train)
 
 # def convert_regressor_output_to_survival_value(output):
